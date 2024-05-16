c2430c29857c6d4ccd434c09f9a8e99508b2496d
louisepbhansen <louise.bp@hotmail.com>
2024-05-16 14:26:42 +0200

    updated setup script

--- setup.sh
+++ setup.sh
@@ -2,7 +2,15 @@ python3 -m venv env
 
 source ./env/bin/activate
 
-pip install scipy==1.10.1
+#pip install scipy==1.10.1
+
+# in case some troubles arises with installing gensim, this should work (scipy dependency broken because of update)
+
+sudo apt update
+sudo apt install libopenblas-dev
+
+# install old version of scipy manually 
+pip install scipy==1.11.0
 
 # install packages from requirements file
 pip install -r requirements.txt
